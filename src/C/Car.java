9686dfb86ad84b004e333894bf1bc88c8aa3cc52
Wassananpanyawan <63122660104@g.lpru.ac.th>
2021-08-05 10:42:46 +0700

    first commit

---
+++ src/C/Car.java
@@ -0,0 +1,25 @@
+package C;
+
+public class Car {
+    private String name = "Honda City";
+    private String model = "2021";
+    private String color = "Blue";
+    private  int tyres = 4;
+
+    void method1(){
+        System.out.println("This car drive Method1");
+    }
+    void method2(){
+        System.out.println("This car drive Method2");
+    }
+    void method3(){
+        System.out.println("This car drive Method3");
+    }
+
+    public static void main(String[] args) {
+        Car Mitsu = new Car();
+        Mitsu.method1();
+        Mitsu.method2();
+        Mitsu.method3();
+    }
+}
